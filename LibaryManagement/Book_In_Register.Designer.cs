6809da7cec61cb27829f46a8faf9109d139b9114
AdorFokir <adorbangalee5@gmail.com>
2019-03-29 00:31:39 +0600

    Book In Register Work Successfully

--- LibaryManagement/Book_In_Register.Designer.cs
+++ LibaryManagement/Book_In_Register.Designer.cs
@@ -47,17 +47,42 @@
             this.libary_ManagementDataSet6 = new LibaryManagement.Libary_ManagementDataSet6();
             this.sectionRackBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.section_RackTableAdapter = new LibaryManagement.Libary_ManagementDataSet6TableAdapters.Section_RackTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.libary_ManagementDataSet7 = new LibaryManagement.Libary_ManagementDataSet7();
+            this.bookInRegisterBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.book_In_RegisterTableAdapter = new LibaryManagement.Libary_ManagementDataSet7TableAdapters.Book_In_RegisterTableAdapter();
+            this.bookDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sectionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.inDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.libary_ManagementDataSet8 = new LibaryManagement.Libary_ManagementDataSet8();
+            this.bookInRegisterBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.book_In_RegisterTableAdapter1 = new LibaryManagement.Libary_ManagementDataSet8TableAdapters.Book_In_RegisterTableAdapter();
+            this.libary_ManagementDataSet9 = new LibaryManagement.Libary_ManagementDataSet9();
+            this.bookInRegisterBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.book_In_RegisterTableAdapter2 = new LibaryManagement.Libary_ManagementDataSet9TableAdapters.Book_In_RegisterTableAdapter();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.roleMasterBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sectionRackBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookInRegisterBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookInRegisterBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookInRegisterBindingSource2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(67, 58);
+            this.label1.Location = new System.Drawing.Point(67, 101);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 0;
@@ -67,7 +92,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(45, 103);
+            this.label2.Location = new System.Drawing.Point(45, 137);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 20);
             this.label2.TabIndex = 1;
@@ -78,7 +103,7 @@
             this.ddlBook.DataSource = this.roleMasterBindingSource;
             this.ddlBook.DisplayMember = "Role";
             this.ddlBook.FormattingEnabled = true;
-            this.ddlBook.Location = new System.Drawing.Point(134, 56);
+            this.ddlBook.Location = new System.Drawing.Point(134, 101);
             this.ddlBook.Name = "ddlBook";
             this.ddlBook.Size = new System.Drawing.Size(198, 21);
             this.ddlBook.TabIndex = 2;
@@ -86,7 +111,7 @@
             // 
             // txtQuantity
             // 
-            this.txtQuantity.Location = new System.Drawing.Point(134, 102);
+            this.txtQuantity.Location = new System.Drawing.Point(134, 139);
             this.txtQuantity.Name = "txtQuantity";
             this.txtQuantity.Size = new System.Drawing.Size(198, 20);
             this.txtQuantity.TabIndex = 3;
@@ -132,42 +157,46 @@
             // btnNew
             // 
             this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNew.Location = new System.Drawing.Point(49, 165);
+            this.btnNew.Location = new System.Drawing.Point(49, 180);
             this.btnNew.Name = "btnNew";
             this.btnNew.Size = new System.Drawing.Size(86, 35);
             this.btnNew.TabIndex = 8;
             this.btnNew.Text = "New";
             this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
             // 
             // btnUpdate
             // 
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdate.Location = new System.Drawing.Point(191, 165);
+            this.btnUpdate.Location = new System.Drawing.Point(200, 180);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(86, 35);
             this.btnUpdate.TabIndex = 9;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Location = new System.Drawing.Point(332, 165);
+            this.btnDelete.Location = new System.Drawing.Point(353, 180);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(86, 35);
             this.btnDelete.TabIndex = 10;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(472, 165);
+            this.btnSave.Location = new System.Drawing.Point(507, 180);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(86, 35);
             this.btnSave.TabIndex = 11;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // libary_ManagementDataSet5
             // 
@@ -197,11 +226,134 @@
             // 
             this.section_RackTableAdapter.ClearBeforeFill = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.bookDataGridViewTextBoxColumn,
+            this.sectionDataGridViewTextBoxColumn,
+            this.quantityDataGridViewTextBoxColumn,
+            this.inDateDataGridViewTextBoxColumn,
+            this.id});
+            this.dataGridView1.DataSource = this.bookInRegisterBindingSource2;
+            this.dataGridView1.Location = new System.Drawing.Point(146, 285);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(488, 150);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // libary_ManagementDataSet7
+            // 
+            this.libary_ManagementDataSet7.DataSetName = "Libary_ManagementDataSet7";
+            this.libary_ManagementDataSet7.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookInRegisterBindingSource
+            // 
+            this.bookInRegisterBindingSource.DataMember = "Book_In_Register";
+            this.bookInRegisterBindingSource.DataSource = this.libary_ManagementDataSet7;
+            // 
+            // book_In_RegisterTableAdapter
+            // 
+            this.book_In_RegisterTableAdapter.ClearBeforeFill = true;
+            // 
+            // bookDataGridViewTextBoxColumn
+            // 
+            this.bookDataGridViewTextBoxColumn.DataPropertyName = "Book";
+            this.bookDataGridViewTextBoxColumn.HeaderText = "Book";
+            this.bookDataGridViewTextBoxColumn.Name = "bookDataGridViewTextBoxColumn";
+            // 
+            // sectionDataGridViewTextBoxColumn
+            // 
+            this.sectionDataGridViewTextBoxColumn.DataPropertyName = "Section";
+            this.sectionDataGridViewTextBoxColumn.HeaderText = "Section";
+            this.sectionDataGridViewTextBoxColumn.Name = "sectionDataGridViewTextBoxColumn";
+            // 
+            // quantityDataGridViewTextBoxColumn
+            // 
+            this.quantityDataGridViewTextBoxColumn.DataPropertyName = "Quantity";
+            this.quantityDataGridViewTextBoxColumn.HeaderText = "Quantity";
+            this.quantityDataGridViewTextBoxColumn.Name = "quantityDataGridViewTextBoxColumn";
+            // 
+            // inDateDataGridViewTextBoxColumn
+            // 
+            this.inDateDataGridViewTextBoxColumn.DataPropertyName = "In_Date";
+            this.inDateDataGridViewTextBoxColumn.HeaderText = "In_Date";
+            this.inDateDataGridViewTextBoxColumn.Name = "inDateDataGridViewTextBoxColumn";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblResult.Location = new System.Drawing.Point(277, 229);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(67, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(134, 68);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(56, 20);
+            this.txtID.TabIndex = 15;
+            // 
+            // libary_ManagementDataSet8
+            // 
+            this.libary_ManagementDataSet8.DataSetName = "Libary_ManagementDataSet8";
+            this.libary_ManagementDataSet8.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookInRegisterBindingSource1
+            // 
+            this.bookInRegisterBindingSource1.DataMember = "Book_In_Register";
+            this.bookInRegisterBindingSource1.DataSource = this.libary_ManagementDataSet8;
+            // 
+            // book_In_RegisterTableAdapter1
+            // 
+            this.book_In_RegisterTableAdapter1.ClearBeforeFill = true;
+            // 
+            // libary_ManagementDataSet9
+            // 
+            this.libary_ManagementDataSet9.DataSetName = "Libary_ManagementDataSet9";
+            this.libary_ManagementDataSet9.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookInRegisterBindingSource2
+            // 
+            this.bookInRegisterBindingSource2.DataMember = "Book_In_Register";
+            this.bookInRegisterBindingSource2.DataSource = this.libary_ManagementDataSet9;
+            // 
+            // book_In_RegisterTableAdapter2
+            // 
+            this.book_In_RegisterTableAdapter2.ClearBeforeFill = true;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
             // Book_In_Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 419);
+            this.ClientSize = new System.Drawing.Size(818, 462);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnUpdate);
@@ -221,6 +373,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.roleMasterBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.sectionRackBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookInRegisterBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookInRegisterBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libary_ManagementDataSet9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookInRegisterBindingSource2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -246,5 +405,23 @@
         private Libary_ManagementDataSet6 libary_ManagementDataSet6;
         private System.Windows.Forms.BindingSource sectionRackBindingSource;
         private Libary_ManagementDataSet6TableAdapters.Section_RackTableAdapter section_RackTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Libary_ManagementDataSet7 libary_ManagementDataSet7;
+        private System.Windows.Forms.BindingSource bookInRegisterBindingSource;
+        private Libary_ManagementDataSet7TableAdapters.Book_In_RegisterTableAdapter book_In_RegisterTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sectionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn inDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label5;
+        private Libary_ManagementDataSet8 libary_ManagementDataSet8;
+        private System.Windows.Forms.BindingSource bookInRegisterBindingSource1;
+        private Libary_ManagementDataSet8TableAdapters.Book_In_RegisterTableAdapter book_In_RegisterTableAdapter1;
+        private Libary_ManagementDataSet9 libary_ManagementDataSet9;
+        private System.Windows.Forms.BindingSource bookInRegisterBindingSource2;
+        private Libary_ManagementDataSet9TableAdapters.Book_In_RegisterTableAdapter book_In_RegisterTableAdapter2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
     }
 }
--- LibaryManagement/Book_In_Register.cs
+++ LibaryManagement/Book_In_Register.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace LibaryManagement
 {
@@ -19,11 +20,81 @@ namespace LibaryManagement
 
         private void Book_In_Register_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'libary_ManagementDataSet9.Book_In_Register' table. You can move, or remove it, as needed.
+            this.book_In_RegisterTableAdapter2.Fill(this.libary_ManagementDataSet9.Book_In_Register);
+            // TODO: This line of code loads data into the 'libary_ManagementDataSet8.Book_In_Register' table. You can move, or remove it, as needed.
+            this.book_In_RegisterTableAdapter1.Fill(this.libary_ManagementDataSet8.Book_In_Register);
+            // TODO: This line of code loads data into the 'libary_ManagementDataSet7.Book_In_Register' table. You can move, or remove it, as needed.
+            this.book_In_RegisterTableAdapter.Fill(this.libary_ManagementDataSet7.Book_In_Register);
             // TODO: This line of code loads data into the 'libary_ManagementDataSet6.Section_Rack' table. You can move, or remove it, as needed.
             this.section_RackTableAdapter.Fill(this.libary_ManagementDataSet6.Section_Rack);
             // TODO: This line of code loads data into the 'libary_ManagementDataSet5.Role_Master' table. You can move, or remove it, as needed.
             this.role_MasterTableAdapter.Fill(this.libary_ManagementDataSet5.Role_Master);
 
+        }
+        //For New
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Save Data
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            AddRecords();
+            lblResult.Text = "Data Save Successfully!!";
+
+        }
+        //Add Data In Database
+        void AddRecords()
+        {
+            Connection con = new Connection();
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Book_In_Register]
+           ([Book]
+           ,[Section]
+           ,[Quantity]
+           ,[In_Date])
+     VALUES
+           ('" + ddlBook.Text + "','" + ddlSection.Text + "','" + txtQuantity.Text + "','"+dateIn.Text+"')", con.ActiveCon());
+            cmd.ExecuteNonQuery();
+            DataTable bt = new DataTable();
+            SqlDataAdapter br = new SqlDataAdapter("select * from Book_In_Register", con.ActiveCon());
+            br.Fill(bt);
+            dataGridView1.DataSource = bt;
+        }
+        //Double Click Data Show
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int n = dataGridView1.SelectedRows[0].Index;
+            ddlBook.SelectedItem = dataGridView1.Rows[n].Cells[0].Value.ToString();
+            ddlSection.SelectedItem = dataGridView1.Rows[n].Cells[1].Value.ToString();
+            txtQuantity.Text = dataGridView1.Rows[n].Cells[2].Value.ToString();
+            dateIn.Text = dataGridView1.Rows[n].Cells[3].Value.ToString();
+           txtID.Text = dataGridView1.Rows[n].Cells[4].Value.ToString();
+        }
+        //Data Update
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Connection con = new Connection();
+            SqlCommand cmd = new SqlCommand(@"UPDATE [dbo].[Book_In_Register]
+   SET[Book] = '" + ddlBook.Text + "',[Section] = '" + ddlSection.Text + "',[Quantity] = '"+txtQuantity.Text+ "',[In_Date] = '"+dateIn.Text+"' WHERE [id] = '" + txtID.Text.ToString() + "'", con.ActiveCon());
+            cmd.ExecuteNonQuery();
+            DataTable bt = new DataTable();
+            SqlDataAdapter br = new SqlDataAdapter("select * from Book_In_Register", con.ActiveCon());
+            br.Fill(bt);
+            dataGridView1.DataSource = bt;
+            lblResult.Text = "Update Successfully!!";
+        }
+        //Data Delete
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Connection con = new Connection();
+            SqlCommand cmd = new SqlCommand(@"Delete From[Book_In_Register] WHERE [id] = '" + txtID.Text + "'", con.ActiveCon());
+            cmd.ExecuteNonQuery();
+            DataTable bt = new DataTable();
+            SqlDataAdapter br = new SqlDataAdapter("select * from Book_In_Register", con.ActiveCon());
+            br.Fill(bt);
+            dataGridView1.DataSource = bt;
+            lblResult.Text = "Delete Successfully!!";
         }
     }
 }

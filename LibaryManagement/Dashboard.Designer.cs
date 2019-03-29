69922c85588cecfdf1df4de60746c7663acf8fc8
Dola <dola.nwu@gmail.com>
2019-03-29 14:16:13 +0600

    Delete Some File

--- LibaryManagement/Dashboard.Designer.cs
+++ LibaryManagement/Dashboard.Designer.cs
@@ -43,8 +43,6 @@
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
@@ -154,35 +152,11 @@
             this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
             this.toolStripStatusLabel.Text = "Status";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(57, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(654, 39);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Welcome To Libray Management System";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(272, 198);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(220, 16);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "For Using This System Go To Menu";
-            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(815, 453);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.menuStrip);
             this.IsMdiContainer = true;
@@ -216,8 +190,6 @@
         private System.Windows.Forms.ToolStripMenuItem bookInRegisterToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem memberTransectionToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem booksOutsideToolStripMenuItem;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
